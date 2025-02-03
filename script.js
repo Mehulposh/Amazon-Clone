38332adef741c1816144d1b24d286c864897ebd4
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-02-03 15:22:49 +0530

    ADDED PRODUCT SEARCH AND DISPLAY FUNCTIONALITY

--- script.js
+++ script.js
@@ -32,3 +32,68 @@ next_Btn.addEventListener("click",(e)=>{
     }
     changeSlide();
 })
+
+
+const searchBox = document.querySelector(".nav-search-input");
+        const searchBtn = document.querySelector(".nav-search-icon");
+        const apiURL = "https://dummyjson.com/products";
+        
+        async function fetchProduct(){
+            try{
+                const rawData = await fetch(apiURL);
+                const prodData = await rawData.json();
+                return prodData.products;
+            }
+            catch(error){
+                console.error("Error fetching the data !");
+            }
+
+        }
+
+        function generateCard(item) {
+            return `
+                <div class="col generated-image">
+                    <h5 class="category">Category : ${item.category}</h5>
+                    <div class="card h-100">
+                        <img src="${item.thumbnail}" class="card-img-top" alt="${item.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${item.title}</h5>
+                            <p class="card-text">${item.price}</p>
+                        </div>
+                    </div>
+                </div>
+  `                   ;
+        }
+
+        async function renderProduct(query){
+            const data = await fetchProduct();
+            const resultContainer = document.querySelector(".box-row.header-box");
+            resultContainer.innerHTML = "";
+            
+            const fillteredData = data.filter(item =>
+            item.title.toLowerCase().includes(query.toLowerCase())
+        );
+
+            if(fillteredData.length>0){
+                fillteredData.forEach(item => {
+                    resultContainer.innerHTML += generateCard(item);
+                });
+            }
+            else{
+                resultContainer.innerHTML = "<p> No Result Found </p>";
+            }
+        }
+
+        searchBtn.addEventListener("click",()=>{
+            
+            const query = searchBox.value.trim();
+            renderProduct(query); 
+        });
+
+        searchBox.addEventListener("keypress",(e)=>{
+            if(e.key === "Enter"){
+                
+                const query = e.target.value.trim();
+                renderProduct(query);
+            }
+        });
